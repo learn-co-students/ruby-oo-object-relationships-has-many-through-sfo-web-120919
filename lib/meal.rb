551a9ec74c82622929d9981c03eedfba81022b1f
github username <github email address>
2019-12-15 14:27:10 -0800

    Done.

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,21 @@
 class Meal
+  attr_reader :customer, :waiter, :total, :tip
 
+  @@all = []
+
+  def initialize(waiter, customer, total, tip)
+    @waiter = waiter
+    @customer = customer
+    @total = total
+    @tip = tip
+    save
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    self.class.all << self
+  end
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,37 @@
 class Waiter
+  attr_reader :name, :age
 
+  @@all = []
+
+  def initialize(name, age)
+    @name = name
+    @age = age
+    save
+  end
+
+  def new_meal(customer, total, tip)
+    Meal.new(self, customer, total, tip)
+  end
+
+  def meals
+    all_meals = Meal.all
+    my_meals = all_meals.select { |meal| meal.waiter == self }
+    my_meals
+  end
+
+  def customers
+    meals.map { |meal| meal.customer }
+  end
+
+  def best_tipper
+    meals.max_by {|meal| meal.tip}.customer 
+  end 
+
+  def self.all
+    @@all
+  end
+
+  def save
+    self.class.all << self
+  end
 end
