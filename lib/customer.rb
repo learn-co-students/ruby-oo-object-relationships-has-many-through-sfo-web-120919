843bc283efd4120b66501de824ee855b0e32c6c5
Donny <edl1155@gmail.com>
2019-12-17 08:32:06 -0800

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,41 @@
 class Customer
+
+  attr_accessor :name, :age
+
+  @@all = []
+
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_meal(waiter, total, tip=0)
+    Meal.new(waiter, self, total, tip)
+  end
+
+  def meals
+    #find a particular customer's meals
+    Meal.all.select do |meal|
+      meal.customer == self
+    end
+  end
   
+  def waiters
+    # meals returned a subset of meals
+    # related to our Customer instance
+    #.  Now we map through to
+    #create a new array of all
+    #the waiters our customers
+    # have been served by.
+    meals.map do |meal|
+      meal.waiter
+    end
+  end
+
+
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,46 @@
 class Waiter
 
+  attr_accessor :name, :yrs_experience
+
+  @@all = []
+
+  def initialize(name, yrs_experience)
+    @name = name
+    @yrs_experience = yrs_experience
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_meal(customer, total, tip=0)
+    Meal.new(self, customer, total, tip)
+  end
+
+  #who tips a waiter the best?
+
+  #1.  First we need to get a list of
+  #all the meals the waiter instance has
+  # served by defining a meals method:
+
+  def meals
+    Meal.all.select do | meal |
+    meal.waiter == self
+    end
+  end
+
+
+#Now that we have an array of all
+#the meals our Waiter instance has served,
+# let's define #best_tipper to find the
+#best tipper
+
+  def best_tipper
+    best_tipped_meal = meals.max do | meal_a, meal_b |
+      meal_a.tip <=> meal_b.tip
+    end
+    best_tipped_meal.customer
+  end
+
 end
