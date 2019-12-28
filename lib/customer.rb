da90add1d1e6f1e1d2f3b8cfcda66be52f94382a
github username <github email address>
2019-12-27 23:28:33 -0800

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,39 @@
 class Customer
+    # A customer has many meals, 
+    # and a customer has many waiters through those meals.
+    # has_many :meals 
+    # has_many :waiters, through: :meals
+
+    attr_accessor :name, :age
+
+    @@all = []
+
+    def initialize(name, age)
+        @name = name
+        @age = age
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(waiter, total, tip=0)
+        Meal.new(waiter, self, total, tip)
+    end
+
+    def meals
+        Meal.all.select do |meal|
+          meal.customer == self #checking against current customer
+        end
+    end
+
+    def waiters
+        meals.map do |meal|
+          meal.waiter
+        end
+    end
+
+    
   
 end
